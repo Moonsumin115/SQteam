4f699817b49ba40f150710174b29528744d5d63d
bluewing115 <smmoon@hancom.com>
2023-10-25 14:35:39 +0900

    PDF열기 오류 스크립트 삭제

--- Test Suites/Unit Test.ts
+++ Test Suites/Unit Test.ts
@@ -11,4 +11,25 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>14eb4384-f6e4-4385-b990-1864f16466c1</testSuiteGuid>
+   <testCaseLink>
+      <guid>08c9c972-1778-4230-b1ca-ac5ae51f72a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/01_Import/DOCX_열기</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>29b09d91-045f-4442-8bd9-709bf714af7c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/01_Import/HWP 열기</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed5acc40-7ac7-43ca-b559-db512c01ed92</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/01_Import/PDF 열기</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
